40319bed32f7733f5f87f08fad78aae1f5c7d544
YurijMoroz2 <139611781+YurijMoroz2@users.noreply.github.com>
2024-03-13 20:11:16 +0200

    fix

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -13,16 +13,17 @@
 // function customError() {
 //   throw new Error('Error');
 // }
-function showMessage(message: string) {
+
+function showMessage(message: string): void {
   console.log(message);
 }
 
 function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
-
-function customError(message: string): never {
-  throw new Error(message);
+function customError(): never {
+  throw new Error('Error');
 }
+
 
 export {};
--- src/generic/3.ts
+++ src/generic/3.ts
@@ -8,7 +8,9 @@
 //   return Object.assign(objA, objB);
 // }
 
-function merge<T, U>(objA: T, objB: U): T & U {
-  return Object.assign({}, objA, objB);
+
+function merge<T extends object, U extends object> (objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
+
 export {}
